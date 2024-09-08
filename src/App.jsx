e4ae3530c18536442401adcdd2d6cd104da439cf
Faraji Ombonya <farajiombonya@gmail.com>
2024-09-08 10:52:56 +0300

    feat: scroll to top functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Landing from "./pages/Landing";
 import ArticleDetail from "./pages/ArticleDetail";
+import ScrollToTop from "./components/ScrollToTop";
 import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <HashRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/">
           <Route index element={<Landing />} />
